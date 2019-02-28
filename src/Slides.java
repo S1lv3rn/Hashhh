1cce4b2c5c51e75f516fd9dbb7a545d356b489ee
jh348 <jh348@pc2-104-l.cs.st-andrews.ac.uk>
2019-02-28 19:44:43 +0000

    I have done slides and photos.

--- src/Slides.java
+++ src/Slides.java
@@ -1,7 +1,34 @@
 public class Slides{
 
     private ArrayList<String> photos = new ArrayList<>();
-    private ArrayList<String> tags = new A
+    private ArrayList<String> tags = new ArrayList<>();
+
+    Slides(ArrayList<String> photos,ArrayList<String> tags){
+        this.photos=photos;
+        this.tags=tags;
+    }
+
+    public ArrayList<String> getPhotos() {
+        return  photos;
+    }
+
+    public ArrayList<String> getTags() {
+        return tags;
+    }
+
+    public void addPhoto(Photos p) {
+        photos.add(p);
+    }
+
+    public boolean CheckSize() {
+        if(photos.size()=<2){
+            return true;
+        }
+
+         else{
+            return false;
+        }
+    }
 
 
 
