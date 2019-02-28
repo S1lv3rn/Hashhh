7575ee220d5e67b04751325a82925d7dc6423550
jojobun <joeyc_1501@hotmail.com>
2019-02-28 18:13:04 +0000

    Hardcoded files in

---
+++ src/Input.java
@@ -0,0 +1,15 @@
+import java.io.File;
+
+public class Input {
+
+    public static void main(String[] args) {
+        String inputA = "/cs/home/jjyc/Documents/hashcode/Hashhh/a_example.txt";
+        String inputB = "/cs/home/jjyc/Documents/hashcode/Hashhh/b_lovely_landscapes.txt";
+        String inputC = "/cs/home/jjyc/Documents/hashcode/Hashhh/c_memorable_moments.txt";
+        String inputD = "/cs/home/jjyc/Documents/hashcode/Hashhh/d_pet_pictures.txt";
+        String inputE = "/cs/home/jjyc/Documents/hashcode/Hashhh/e_shiny_selfies.txt";
+        File file = new File(args[0]);
+
+    }
+
+}
